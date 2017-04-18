7b1409ededf4ad1a15105eabf224152fdf22e708
calaca <lorena.calaca.od@gmail.com>
2017-04-18 13:41:02 -0300

    tests: ensures all feeds have a name defined and not empty, closes #4

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -39,10 +39,16 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* DONE: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('should have a name defined, it should not be empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe(0);
+            });
+        });
     });
 
 
